a55e38d62369a20181ffb3752af5c28e4a9ef971
Andrés Sneer <andresduranrod@gmail.com>
2023-05-21 14:01:18 -0600

    Threads optimization (#2)
    * Joining threads
    
    * Improving times for face recognition
    
    * increasing delay for rtsp and adding garbage collector
    
    * adding method to save face detected
    
    * adding margin by face size
    
    * adding wifi image

--- libs/face_detection.py
+++ libs/face_detection.py
@@ -1,6 +1,7 @@
 import cv2
 import psutil
 import time
+from datetime import datetime
 import os
 from .smartmirror_utils import log
 
@@ -51,12 +52,19 @@ class FaceDetector:
         if(self.faces_count_verification(close_detected_faces)):
             return close_detected_faces
         else: return []
+
+    def get_minimun_face_margin(self,camera_size,face_size):
+        extra_size = camera_size - face_size
+        margin_size = extra_size/4
+        return margin_size
     
     def get_close_detected_faces(self,faces,frame):
         close_detected_faces = []
         for (x, y, w, h) in faces:
             if ((w > frame.shape[1]*self.percent_to_detect) or (h > frame.shape[0]*self.percent_to_detect)):
-                close_detected_faces.append((x, y, w, h,(0, 255, 0)))
+                width_minimun_margin = self.get_minimun_face_margin(frame.shape[1],w)
+                if(x >= width_minimun_margin and (x+w <= frame.shape[1]-width_minimun_margin)):
+                    close_detected_faces.append((x, y, w, h,(0, 255, 0)))
         return close_detected_faces
     
     def faces_count_verification(self,close_detected_faces):
@@ -83,7 +91,10 @@ class FaceDetectorApp:
                 self.is_new_person_detected = False
                 return
             faces = self.face_detector.detect_faces(frame)
+            person_detected_old = self.is_new_person_detected
             self.is_new_person_detected = self.is_person_detected(faces)
+            if(self.is_new_person_detected and person_detected_old != self.is_new_person_detected):
+                self.save_face_image(frame,faces)
             #self.monitor_cpu()
             if cv2.waitKey(1) & 0xFF == ord('q'): return
             cv2.setWindowProperty('Video', cv2.WND_PROP_VSYNC, 0)
@@ -104,6 +115,19 @@ class FaceDetectorApp:
         cpu_usage = psutil.cpu_percent()
         print(f"Uso de CPU: {cpu_usage}%")
         self.cpu_delay_counter = 0
+
+    def draw_faces(self,frame,faces):
+        for (x, y, w, h, color) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+        return frame
+
+    def save_face_image(self,frame,faces):
+        frame = self.draw_faces(frame,faces)
+        localPathImages = f"{os.path.dirname(os.path.realpath(__file__))}/"
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d-%H-%M-%S")
+        file_name = f"{localPathImages}face-{dt_string}.jpg"
+        cv2.imwrite(file_name, frame)
 
 if __name__ == '__main__':
     app = FaceDetectorApp()
